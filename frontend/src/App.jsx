843c10c74c4c8975d87fa12a8adbd5f95bb0b4a8
ravi <patodiyaravi680@gmail.com>
2025-05-04 18:31:31 +0530

    admin dashboard complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,12 @@ import MainLayout from "./layout/MainLayout";
 import Courses from "./pages/students/Courses";
 import MyLearning from "./pages/students/MyLearning";
 import Profile from "./pages/students/Profile";
+import Dashboard from "./pages/admin/Dashboard";
+import Sidebar from "./pages/admin/Sidebar";
+import CourseTab from "./pages/admin/course/CourseTab";
+import CourseTable from "./pages/admin/course/CourseTable";
+import AddCourse from "./pages/admin/course/AddCourse";
+import EditCourse from "./pages/admin/course/EditCourse";
 
 const appRouter = createBrowserRouter([
   {
@@ -21,17 +27,39 @@ const appRouter = createBrowserRouter([
         ),
       },
       {
-        path:"login",
-        element:(<Login />)
+        path: "login",
+        element: <Login />,
       },
       {
-        path:"my-learning",
-        element:(<MyLearning />)
+        path: "my-learning",
+        element: <MyLearning />,
       },
       {
-        path:"profile",
-        element:(<Profile />)
-      }
+        path: "profile",
+        element: <Profile />,
+      },
+      {
+        path: "admin",
+        element: <Sidebar />,
+        children: [
+          {
+            path: "",
+            element: <Dashboard />,
+          },
+          {
+            path:'course',
+            element: <CourseTable />
+          },
+          {
+            path:"course/add-course",
+            element:<AddCourse />
+          },
+          {
+            path:"course/edit/:courseID",
+            element:<EditCourse />
+          },
+        ],
+      },
     ],
   },
 ]);
---
+++ frontend/src/pages/admin/course/CourseTab.jsx
@@ -0,0 +1,162 @@
+// import RichTextEditor from "@/components/RichTextEditor";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Loader2 } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "sonner";
+
+const CourseTab = () => {
+  return (
+    <Card>
+      <CardHeader className="flex flex-row justify-between">
+        <div>
+          <CardTitle>Basic Course Information</CardTitle>
+          <CardDescription>
+            Make changes to your courses here. Click save when you're done.
+          </CardDescription>
+        </div>
+        <div className="space-x-2">
+          <Button
+            // disabled={courseByIdData?.course.lectures.length === 0}
+            variant="outline"
+            
+          >
+            {true ? "Unpublished" : "Publish"}
+          </Button>
+          <Button>Remove Course</Button>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <div className="space-y-4 mt-5">
+          <div>
+            <Label>Title</Label>
+            <Input
+              type="text"
+              name="courseTitle"
+              placeholder="Ex. Fullstack developer"
+            />
+          </div>
+          <div>
+            <Label>Subtitle</Label>
+            <Input
+              type="text"
+              name="subTitle"
+              placeholder="Ex. Become a Fullstack developer from zero to hero in 2 months"
+            />
+          </div>
+          <div>
+            <Label>Description</Label>
+            {/* <RichTextEditor  /> */}
+          </div>
+          <div className="flex items-center gap-5">
+            <div>
+              <Label>Category</Label>
+              <Select
+              >
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Select a category" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Category</SelectLabel>
+                    <SelectItem value="Next JS">Next JS</SelectItem>
+                    <SelectItem value="Data Science">Data Science</SelectItem>
+                    <SelectItem value="Frontend Development">
+                      Frontend Development
+                    </SelectItem>
+                    <SelectItem value="Fullstack Development">
+                      Fullstack Development
+                    </SelectItem>
+                    <SelectItem value="MERN Stack Development">
+                      MERN Stack Development
+                    </SelectItem>
+                    <SelectItem value="Javascript">Javascript</SelectItem>
+                    <SelectItem value="Python">Python</SelectItem>
+                    <SelectItem value="Docker">Docker</SelectItem>
+                    <SelectItem value="MongoDB">MongoDB</SelectItem>
+                    <SelectItem value="HTML">HTML</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+            <div>
+              <Label>Course Level</Label>
+              <Select
+              >
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Select a course level" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Course Level</SelectLabel>
+                    <SelectItem value="Beginner">Beginner</SelectItem>
+                    <SelectItem value="Medium">Medium</SelectItem>
+                    <SelectItem value="Advance">Advance</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+            <div>
+              <Label>Price in (INR)</Label>
+              <Input
+                type="number"
+                name="coursePrice"
+                placeholder="199"
+                className="w-fit"
+              />
+            </div>
+          </div>
+          <div>
+            <Label>Course Thumbnail</Label>
+            <Input
+              type="file"
+              accept="image/*"
+              className="w-fit"
+            />
+            {true && (
+              <img
+                className="e-64 my-2"
+                alt="Course Thumbnail"
+              />
+            )}
+          </div>
+          <div>
+            <Button onClick={() => navigate("/admin/course")} variant="outline">
+              Cancel
+            </Button>
+            <Button >
+              {false ? (
+                <>
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  Please wait
+                </>
+              ) : (
+                "Save"
+              )}
+            </Button>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default CourseTab;
